06ee0e4dd125b2222db427c159cfa76e0338cd6b
MasterOfTG <85876629+MasterOfTG@users.noreply.github.com>
2021-12-14 14:38:39 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -172,7 +172,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         elif query.data == "help_data":
             await query.answer()
             keyboard = InlineKeyboardMarkup([
-                [InlineKeyboardButton("🔐 Close", callback_data="close"),
+                [InlineKeyboardButton("♻️ GROUP", url="https://telegram.dog/MOvieJunctionGrp"),
                     InlineKeyboardButton("ABOUT 🚩", callback_data="about_data")],
                 [InlineKeyboardButton("🔻 FEEDBACKS & SUGGESTIONS 🔻", url="https://telegram.dog/Mj_Chats")]
                   
@@ -189,7 +189,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             await query.answer()
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("⚡ HELP", callback_data="help_data"),
-                    InlineKeyboardButton("Close 🔐", callback_data="close")],
+                    InlineKeyboardButton("CHANNEL 🀄", url="https://telegram.dog/Mj_Linkz")],
                 [InlineKeyboardButton("⚙️ SOURCE CODE ⚙️", url="https://t.me/MvJnAdmin/4")]
             ])
 
